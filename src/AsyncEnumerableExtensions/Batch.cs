117b345baa1426bee5bb44638f4cdd6050799931
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-10-28 11:18:33 -0500

    Added IAsyncEnumerable.Batch extension method

---
+++ src/AsyncEnumerableExtensions/Batch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using CommunityToolkit.Diagnostics;
+
+// ReSharper disable once CheckNamespace
+namespace OwlCore.Extensions
+{
+    /// <summary>
+    /// Extension methods related to IAsyncEnumerable{T}.
+    /// </summary>
+    public static partial class AsyncEnumerableExtensions
+    {
+        /// <summary>
+        /// Yields <typeparam name="TSource"/> in batches instead of yielding each individual item.
+        /// </summary>
+        /// <param name="sources">The source to enumerate for batching.</param>
+        /// <param name="batchSize">The size of each batch.</param>
+        /// <param name="cancellationToken">A cancellation token that can be used to cancel the work if it has not yet started.</param>
+        public static async IAsyncEnumerable<IList<TSource>> Batch<TSource>(this IAsyncEnumerable<TSource> sources, int batchSize, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            Guard.IsGreaterThan(value: batchSize, minimum: 0);
+
+            var currentBatch = new List<TSource>();
+
+            await foreach (var item in sources.WithCancellation(cancellationToken))
+            {
+                currentBatch.Add(item);
+
+                if (currentBatch.Count == batchSize)
+                {
+                    yield return currentBatch;
+                    currentBatch = new();
+                }
+            }
+
+            // If the batch has items, they were added without being yielded.
+            if (currentBatch.Count > 0)
+            {
+                yield return currentBatch;
+            }
+        }
+    }
+}
---
+++ tests/AsyncEnumerableExtensions.cs
@@ -0,0 +1,62 @@
+﻿namespace OwlCore.Extensions.Tests
+{
+    [TestClass]
+    public class AsyncEnumerableExtensions
+    {
+        [DataRow(1000, 100)]
+        [DataRow(100, 20)]
+        [DataRow(100, 5)]
+        [DataRow(100, 5)]
+        [DataRow(100, 1)]
+        [TestMethod, Timeout(1000)]
+        public async Task BatchSize(int total, int batchSize)
+        {
+            await foreach (var batch in AsyncEnumerable.Range(0, total).Batch(batchSize))
+            {
+                Assert.IsTrue(batchSize == batch.Count);
+            }
+        }
+
+        [DataRow(1000, 100)]
+        [DataRow(5, 3)]
+        [DataRow(100, 3)]
+        [DataRow(100, 3)]
+        [TestMethod, Timeout(1000)]
+        public async Task BatchCount(int total, int batchSize)
+        {
+            var expectedBatchCount = Math.Ceiling((double)total / batchSize);
+            var batchesEmitted = 0;
+
+            await foreach (var _ in AsyncEnumerable.Range(0, total).Batch(batchSize))
+                batchesEmitted++;
+
+            Assert.AreEqual(expectedBatchCount, batchesEmitted);
+        }
+
+        [TestMethod, DataRow(0, -1)]
+        [DataRow(0, 0)]
+        public async Task PositiveBatchSizeOnly(int total, int batchSize)
+        {
+            await Assert.ThrowsExceptionAsync<ArgumentOutOfRangeException>(async () => await AsyncEnumerable.Range(0, total).Batch(batchSize).ToListAsync());
+        }
+
+        [DataRow(1000, 100)]
+        [DataRow(100, 20)]
+        [DataRow(100, 5)]
+        [DataRow(100, 5)]
+        [DataRow(100, 1)]
+        [TestMethod, Timeout(1000)]
+        public async Task BatchOrder(int total, int batchSize)
+        {
+            var currentNumber = 0;
+
+            await foreach (var batch in AsyncEnumerable.Range(0, total).Batch(batchSize))
+            {
+                foreach (var item in batch)
+                {
+                    Assert.AreEqual(expected: currentNumber++, actual: item);
+                }
+            }
+        }
+    }
+}
