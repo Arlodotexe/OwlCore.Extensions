c0b8e55a64dac62594ef7ed066bcd909de89568b
Arlo Godfrey <arlo.godfrey@outlook.com>
2024-04-27 09:03:31 -0500

    Release 0.8.0. Breaking fixes to Stream.ToBytes and Stream.ToBytesAsync.
    [Breaking]
    The Stream.ToBytes and Stream.ToBytesAsync method no longer try to read position or seek the stream internally. Instead, rewind the stream before using this extension method. This allows us to support non-seekable streams.
    
    [Improvements]
    Stream.ToBytesAsync now properly takes a cancellation token for the second parameter.

--- src/StreamExtensions/ToBytes.cs
+++ src/StreamExtensions/ToBytes.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 // ReSharper disable once CheckNamespace
@@ -14,51 +14,25 @@ namespace OwlCore.Extensions
         /// Converts the <paramref name="input"/> <see cref="Stream"/> to a byte array.
         /// </summary>
         /// <remarks>
-        /// The method will seek to the start of the stream, read (and copy into a MemoryStream) until it runs out of data. It then asks the MemoryStream to return a copy of the data in an array.
+        /// The method will read and copy into a MemoryStream until it runs out of data. It then asks the MemoryStream to return a copy of the data in an array.
         /// </remarks>
         public static byte[] ToBytes(this Stream input)
         {
-            var originalPosition = input.Position;
-            if (input.Position != 0)
-            {
-                if (input.CanSeek)
-                    input.Position = 0;
-                else
-                    throw new ArgumentException("Stream is not at position 0 and is unable to seek.", nameof(input));
-            }
-
             using var memStream = new MemoryStream();
             input.CopyTo(memStream);
-
-            if (input.CanSeek)
-                input.Position = originalPosition;
-
             return memStream.ToArray();
         }
 
         /// <summary>
-        /// Converts the <paramref name="input"/> <see cref="Stream"/> to a byte array.
+        /// Converts the <paramref name="input"/> <see cref="Stream"/> to a byte array asynchronously.
         /// </summary>
         /// <remarks>
-        /// The method will seek to the start of the stream, read (and copy into a MemoryStream) until it runs out of data. It then asks the MemoryStream to return a copy of the data in an array.
+        /// The method will read and copy into a MemoryStream until it runs out of data. It then asks the MemoryStream to return a copy of the data in an array.
         /// </remarks>
-        public static async Task<byte[]> ToBytesAsync(this Stream input)
+        public static async Task<byte[]> ToBytesAsync(this Stream input, CancellationToken cancellationToken = default)
         {
-            var originalPosition = input.Position;
-            if (input.Position != 0)
-            {
-                if (input.CanSeek)
-                    input.Position = 0;
-                else
-                    throw new ArgumentException("Stream is not at position 0 and is unable to seek.", nameof(input));
-            }
-
             using var memStream = new MemoryStream();
-            await input.CopyToAsync(memStream);
-
-            if (input.CanSeek)
-                input.Position = originalPosition;
-
+            await input.CopyToAsync(memStream, cancellationToken);
             return memStream.ToArray();
         }
     }
